0d430955175977deaff2a2df060352b076cb4cba
Gaëtan Gelineau <gaetan.gelineau@gmail.com>
2022-07-31 16:34:05 +0200

    feat: Add common module with idGeneratorService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExerciseModule } from './exercise/exercise.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ExerciseModule],
+  imports: [ExerciseModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
