6523e4a0a30e34126741e79b44c9cf13c0cad5ec
Gaetan <gaetan.gelineau@gmail.com>
2022-12-30 19:17:26 +0100

    feat: sessionCreation (still wip)

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -7,37 +7,109 @@ import {
   SessionStep,
   TimeExercise,
 } from './types/session';
+import { SessionType } from './types/sessionType';
 
 @Injectable()
 export class SessionService {
   constructor(private readonly exerciseRepository: ExerciseRepository) {}
 
-  async getSessionProposition(
-    dto: GetSessionPropositionDTO,
-  ): Promise<SessionStep[]> {
-    const exercises = await this.exerciseRepository.findAll();
-    const selectedExercises = exercises.sort(() => 0.5 - Math.random());
+  async getSessionProposition({
+    sessionType,
+    intensity,
+    exerciseDifficulty,
+  }: GetSessionPropositionDTO): Promise<SessionStep[]> {
+    const seriesNumber = 3;
+    const exercisesPerSeries = Math.round(Math.random() * 4 + 6);
+    const breakTimeBetweenSeries = Math.round(Math.random() * 39 + 90);
+    const sessionStructure = ['Cardio', 'Jambes', 'Abs', 'Gainage'];
+    const minDifficulty = Math.max(exerciseDifficulty - 1, 1);
+    const maxDifficulty = Math.min(exerciseDifficulty + 1, 5);
+    const isTimeExercise = sessionType === SessionType.TABATA;
 
-    const session = selectedExercises.flatMap<SessionStep>(
-      (exercise, index): SessionStep[] => {
-        const exerciseStep: TimeExercise | RepetitionsExercise =
-          exercise.isTimeExercise
-            ? {
-                type: 'timeExercise',
-                duration: 30,
-                exercise,
-              }
-            : { type: 'repetitionsExercise', repetitions: 10, exercise };
-
-        const breakStep: Break = { type: 'break', duration: 30 };
-
-        if (index === 0) {
-          return [exerciseStep];
-        }
-
-        return [breakStep, exerciseStep];
-      },
+    const allExercises = await this.exerciseRepository.findAll();
+    const selectedExercises = allExercises.filter(
+      (exercise) =>
+        exercise.difficulty >= minDifficulty &&
+        exercise.difficulty <= maxDifficulty &&
+        exercise.isTimeExercise === isTimeExercise,
     );
+    const exercisesByCategory = sessionStructure.reduce(
+      (acc, category) => ({
+        ...acc,
+        [category]: selectedExercises.filter((exercise) =>
+          exercise.categories
+            .map((exerciseCategory) => exerciseCategory.name)
+            .includes(category),
+        ),
+      }),
+      {},
+    );
+
+    console.dir(exercisesByCategory, { depth: null });
+
+    const restTime = Math.round(Math.random() * 6 + 20);
+    const effortTime = Math.round(Math.random() * 6 + 20);
+    const serieFormat = {
+      [SessionType.TABATA]: {
+        1: { effortTime, restTime },
+        2: { effortTime: effortTime + 2, restTime: restTime - 2 },
+        3: { effortTime: effortTime + 4, restTime: restTime - 4 },
+      },
+      [SessionType.SERIES]: {
+        1: {
+          repetitions: Math.round(Math.random() * 5 + 6),
+          restTime: Math.round(restTime / 2),
+        },
+        2: {
+          repetitions: Math.round(Math.random() * 3 + 10),
+          restTime: Math.round(restTime / 2),
+        },
+        3: {
+          repetitions: Math.round(Math.random() * 6 + 12),
+          restTime: Math.round(restTime / 2),
+        },
+      },
+    };
+
+    const serie = Array.from({
+      length: exercisesPerSeries,
+    }).flatMap<SessionStep>((_, index): SessionStep[] => {
+      const category = sessionStructure[index % sessionStructure.length];
+      const exercise =
+        exercisesByCategory[category][
+          Math.floor(Math.random() * exercisesByCategory[category].length)
+        ];
+
+      const exerciseStep: TimeExercise | RepetitionsExercise = {
+        type: exercise.isTimeExercise ? 'timeExercise' : 'repetitionsExercise',
+        exercise,
+        ...(exercise.isTimeExercise && {
+          duration: serieFormat.tabata[intensity].effortTime,
+        }),
+        ...(!exercise.isTimeExercise && {
+          repetitions: serieFormat.series[intensity].repetitions,
+        }),
+      };
+
+      const breakStep: Break = {
+        type: 'break',
+        duration: serieFormat[sessionType][intensity].restTime,
+      };
+
+      if (index === 0) {
+        return [exerciseStep];
+      }
+
+      return [breakStep, exerciseStep];
+    });
+
+    const breakBetweenSeries: Break = {
+      type: 'break',
+      duration: breakTimeBetweenSeries,
+    };
+    const session = Array.from({ length: seriesNumber })
+      .flatMap(() => [...serie, breakBetweenSeries])
+      .slice(0, -1);
 
     return session;
   }
